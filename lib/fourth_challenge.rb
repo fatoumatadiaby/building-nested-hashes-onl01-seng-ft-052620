00d34c29114e8f38945462b823dd498d95bf60f1
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-04 15:39:02 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,7 +21,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => (
         {
         :name => "Steven",
         :age => "30",
@@ -31,7 +31,7 @@ def fourth_challenge
         :name => "Nurse",
         :age => "44",
         :attitude => "worried"
-      ]
+      )
    }
 }
 
