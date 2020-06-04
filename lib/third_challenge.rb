36031ce75e7f78bf7cbf5a5c98af364894fa001e
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-04 15:21:21 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,2 +1,38 @@
 def third_challenge
   epic_tragedy = {
+:montague => {
+    :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53",
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => []
+  }
+
+# ^ right here after this curly bracket
+# as of now it just goes straight to :capulet and gets confused
+  
+   :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+   
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },  
+      :heroine_friends => []
+   }
