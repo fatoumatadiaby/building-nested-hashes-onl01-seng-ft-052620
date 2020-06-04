df31be9d0174d35a9a498b7818f76d74d0f4d10b
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-04 15:41:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,9 +1,9 @@
 def third_challenge
   epic_tragedy = {
-:montague => {
+  :montague => {
     :patriarch => {  
       :name => "Lord Montague",
-      :age => "53",
+      :age => "53"
   },
     :matriarch => {
       :name => "Lady Montague",
@@ -11,18 +11,26 @@ def third_challenge
   },
     :hero => {
       :name => "Romeo",
-      :age => "15",
-      :status => "alive"
+      :age => "15"
   },
-    :hero_friends => []
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    ]
   },
-  
-   :capulet => {
+  :capulet => {
     :patriarch => {
       :name => "Lord Capulet",
       :age => "50"
     },
-   
     :matriarch => {
       :name => "Lady Capulet",
       :age => "51"
@@ -31,8 +39,18 @@ def third_challenge
       :name => "Juliet",
       :age => "15",
       :status => "alive"
-    },  
-      :heroine_friends => []
-   }
-  } 
-end
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
